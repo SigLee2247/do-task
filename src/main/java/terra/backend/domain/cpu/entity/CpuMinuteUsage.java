8082b07f5dbb3010dda9363bf754c5d142269bcc
Siglee2247 <lgh941107@gmail.com>
2024-05-23 13:58:43 +0900

    feat: 엔티티 생성 #1 - 시간 별 CPU 사용량 - 일별 CPU 사용량 - 분별 CPU 사용량

---
+++ src/main/java/terra/backend/domain/cpu/entity/CpuMinuteUsage.java
@@ -0,0 +1,26 @@
+package terra.backend.domain.cpu.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class CpuMinuteUsage {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(updatable = false, name = "cpu_minute_usage_id")
+  private Long id;
+
+  @Column(nullable = false)
+  private int usage;
+
+  @Column(nullable = false)
+  private LocalDateTime samplingDate;
+}
